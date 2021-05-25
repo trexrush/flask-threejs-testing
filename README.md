9f590cb63aa353e4dcab33147ef18312cd970c3e
Eduardo Mazuera <edmazuera@gmail.com>
2021-05-25 14:58:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# random-art
+ The purpose of this project is to learn Flask, React, and Three.js, but it is in its infancy at the moment.
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)     
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<name>')
+def return_name(name):
+    NUM_VARIABLES = 3
+    if type(name) != int:
+        name = hash(name)
+    random.seed(name)
+    seeds = [0] * (NUM_VARIABLES + 1)
+    seeds[0] = name
+    for i in range (NUM_VARIABLES + 1):
+        seeds[i] = random.random() # create values from the seed
+
+    return render_template('seed.html', seed = seeds)
+    # return "Hello, {}".format(name)
+
+
+if __name__ == "__main__": # if you use flask.run you dont need this
+    app.run(debug=True)
